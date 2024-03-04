5bc176c49a064117cec15962a38fb27d5d25d5ba
Daniel Bohorquez <>
2024-03-03 22:01:56 -0500

    feat: Update fonts + Fixed components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { OffcanvasComponent } from './offcanvas/offcanvas.component';
+import { HeroComponent } from './hero/hero.component';
+import { PromoBannerComponent } from './promo-banner/promo-banner.component';
+import { OffersComponent } from './offers/offers.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, OffcanvasComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HeroComponent, PromoBannerComponent, OffersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
